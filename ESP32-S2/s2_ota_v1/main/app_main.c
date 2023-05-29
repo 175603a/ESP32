acf126457e0f99f152eec624549f3224f4125436
175603a <244439775@qq.com>
2023-05-29 13:16:54 +0800

    目前还有json解析失败的问题，应该是FreeRTOS消息队列接收问题

--- ESP32-S2/s2_ota_v1/main/app_main.c
+++ ESP32-S2/s2_ota_v1/main/app_main.c
@@ -3,8 +3,18 @@
 
 static const char *TAG = "main";
 
+void printTask(void *pvParameters)
+{
+    while (1)
+    {
+        ESP_LOGI(TAG, "Tis is ota version 1.0.2");
+        vTaskDelay(5000 / portTICK_PERIOD_MS);
+    }
+}
+
 void app_main(void)
 {
     ESP_ERROR_CHECK(nvs_flash_init());
     wifi_smartconfig();
+    xTaskCreate(printTask, "printTask", 2048, NULL, 5, NULL);
 }
